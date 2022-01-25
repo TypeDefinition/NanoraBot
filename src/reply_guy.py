2472c1e8f568bac7e478b4e1da13ce5fe3fde2c0
TypeDefinition <lnxterry@gmail.com>
2022-01-25 10:45:52 +0800

    Updated messages

--- src/reply_guy.py
+++ src/reply_guy.py
@@ -1,7 +1,7 @@
 REPLY_GUY_BOT_NAME = "reply-guy-bot"
 REPLY_GUY_MESSAGES = ["*Gasp!* Me, a bot? Never nanora! 有りえないのら!", \
         "NanoraBot is bot? No! I'm adult sexy wonderful beautiful cute えーと、 adult sexy NanoraBot. てへぺろ!", \
-        "NanoraBot is cutest beautiful genius sexy sexy adult wonderful great exellent perfect NanoraBot. えへへ!"]
+        "No! No bot! NanoraBot is cutest beautiful genius sexy sexy adult wonderful great exellent perfect NanoraBot. えへへ!"]
 
 def get_reply_guy_message():
     return REPLY_GUY_MESSAGES[random.randrange(0, len(REPLY_GUY_MESSAGES))]
