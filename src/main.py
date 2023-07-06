32b765167257b46e5a27e0772138d26f86753c6a
Lim Ngian Xin Terry <lnxterry@gmail.com>
2023-07-06 23:08:48 +0800

    Increase wait time

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ from pekonora import get_pekonora_message
 
 # Constants
 BOOT_DURATION = 5
-WAIT_DURATION = 10
+WAIT_DURATION = 60
 SPAM_LIMIT = 5
 PARENT_DIR = str(pathlib.Path(os.path.abspath(__file__)).parents[1])
 SAVE_FILE = PARENT_DIR + "/replied_posts.json"
